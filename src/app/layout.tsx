bd1540ce626947d79ce87c91312ce64ce7eaad9c
junghongseop <a01082372487@gmail.com>
2024-08-24 23:07:58 +0900

    init

---
+++ src/app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "정홍섭의 개발 블로그",
+  description: "정홍섭이 만든 개발 블로그입니다.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
